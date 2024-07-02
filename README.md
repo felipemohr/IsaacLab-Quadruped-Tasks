829c727c2f8c2cd2083bc4c8948ac7ac54a96da7
Felipe Mohr <felipe18mohr@gmail.com>
2024-07-02 12:01:07 -0300

    First commit. Add IsaacLab Extension template

---
+++ README.md
@@ -0,0 +1,13 @@
+# Isaac Lab Quadruped Tasks Extension
+
+[![IsaacSim](https://img.shields.io/badge/IsaacSim-4.0.0-silver.svg)](https://docs.omniverse.nvidia.com/isaacsim/latest/overview.html)
+[![Isaac Lab](https://img.shields.io/badge/IsaacLab-1.0.0-silver)](https://isaac-sim.github.io/IsaacLab)
+[![Python](https://img.shields.io/badge/python-3.10-blue.svg)](https://docs.python.org/3/whatsnew/3.10.html)
+[![Linux platform](https://img.shields.io/badge/platform-linux--64-orange.svg)](https://releases.ubuntu.com/20.04/)
+[![Windows platform](https://img.shields.io/badge/platform-windows--64-orange.svg)](https://www.microsoft.com/en-us/)
+[![pre-commit](https://img.shields.io/badge/pre--commit-enabled-brightgreen?logo=pre-commit&logoColor=white)](https://pre-commit.com/)
+[![License](https://img.shields.io/badge/license-MIT-yellow.svg)](https://opensource.org/license/mit)
+
+## Overview
+
+This repository contains a Quadruped Tasks extension based on Isaac Lab.
---
+++ exts/omni.isaac.lab_quadruped_tasks/setup.py
@@ -0,0 +1,67 @@
+"""Installation script for the 'omni.isaac.lab_quadruped_tasks' python package."""
+
+import itertools
+import os
+import platform
+import toml
+
+from setuptools import setup
+
+# Obtain the extension data from the extension.toml file
+EXTENSION_PATH = os.path.dirname(os.path.realpath(__file__))
+# Read the extension.toml file
+EXTENSION_TOML_DATA = toml.load(os.path.join(EXTENSION_PATH, "config", "extension.toml"))
+
+# Minimum dependencies required prior to installation
+INSTALL_REQUIRES = [
+    # generic
+    "numpy",
+    "torch==2.2.2",
+    "torchvision>=0.14.1",  # ensure compatibility with torch 1.13.1
+    # 5.26.0 introduced a breaking change, so we restricted it for now.
+    # See issue https://github.com/tensorflow/tensorboard/issues/6808 for details.
+    "protobuf>=3.20.2, < 5.0.0",
+    # data collection
+    "h5py",
+    # basic logger
+    "tensorboard",
+    # video recording
+    "moviepy",
+]
+
+PYTORCH_INDEX_URL = ["https://download.pytorch.org/whl/cu118"]
+
+# Extra dependencies for RL agents
+EXTRAS_REQUIRE = {
+    "rsl-rl": ["rsl-rl@git+https://github.com/leggedrobotics/rsl_rl.git"],
+}
+# Add the names with hyphens as aliases for convenience
+EXTRAS_REQUIRE["rsl_rl"] = EXTRAS_REQUIRE["rsl-rl"]
+
+# Cumulation of all extra-requires
+EXTRAS_REQUIRE["all"] = list(itertools.chain.from_iterable(EXTRAS_REQUIRE.values()))
+# Remove duplicates in the all list to avoid double installations
+EXTRAS_REQUIRE["all"] = list(set(EXTRAS_REQUIRE["all"]))
+
+# Installation operation
+setup(
+    name="omni.isaac.lab_quadruped_tasks",
+    packages=["omni.isaac.lab_quadruped_tasks"],
+    author=EXTENSION_TOML_DATA["package"]["author"],
+    maintainer=EXTENSION_TOML_DATA["package"]["maintainer"],
+    url=EXTENSION_TOML_DATA["package"]["repository"],
+    version=EXTENSION_TOML_DATA["package"]["version"],
+    description=EXTENSION_TOML_DATA["package"]["description"],
+    keywords=EXTENSION_TOML_DATA["package"]["keywords"],
+    install_requires=INSTALL_REQUIRES,
+    license="MIT",
+    include_package_data=True,
+    python_requires=">=3.10",
+    classifiers=[
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.10",
+        "Isaac Sim :: 2023.1.1",
+        "Isaac Sim :: 4.0.0",
+    ],
+    zip_safe=False,
+)
