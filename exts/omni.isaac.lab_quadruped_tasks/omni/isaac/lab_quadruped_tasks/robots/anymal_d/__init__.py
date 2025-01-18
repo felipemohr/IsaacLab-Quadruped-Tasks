5dc072b3df6b17373fbc4e1f096a7f45da6fb816
Felipe Mohr <felipe18mohr@gmail.com>
2025-01-18 14:38:13 -0300

    Rename anymal_d tasks and create new tasks for CPG

--- exts/omni.isaac.lab_quadruped_tasks/omni/isaac/lab_quadruped_tasks/robots/anymal_d/__init__.py
+++ exts/omni.isaac.lab_quadruped_tasks/omni/isaac/lab_quadruped_tasks/robots/anymal_d/__init__.py
@@ -9,149 +9,168 @@ from omni.isaac.lab_quadruped_tasks.agents.rsl_rl_cfg import QuadrupedPPORunnerC
 from . import anymal_d_env_cfg
 
 
+############################
+# AnymalD Joints Environment
+############################
+
 ##
 # Create PPO runners for RSL-RL
 ##
 
-anymal_d_blind_flat_runner_cfg = QuadrupedPPORunnerCfg()
-anymal_d_blind_flat_runner_cfg.experiment_name = "anymal_d_blind_flat"
+anymal_d_joints_blind_flat_runner_cfg = QuadrupedPPORunnerCfg()
+anymal_d_joints_blind_flat_runner_cfg.experiment_name = "anymal_d_joints_blind_flat"
 
-anymal_d_blind_rough_runner_cfg = QuadrupedPPORunnerCfg()
-anymal_d_blind_rough_runner_cfg.experiment_name = "anymal_d_blind_rough"
+anymal_d_joints_blind_rough_runner_cfg = QuadrupedPPORunnerCfg()
+anymal_d_joints_blind_rough_runner_cfg.experiment_name = "anymal_d_joints_blind_rough"
 
-anymal_d_blind_stairs_runner_cfg = QuadrupedPPORunnerCfg()
-anymal_d_blind_stairs_runner_cfg.experiment_name = "anymal_d_blind_stairs"
+anymal_d_joints_blind_stairs_runner_cfg = QuadrupedPPORunnerCfg()
+anymal_d_joints_blind_stairs_runner_cfg.experiment_name = "anymal_d_joints_blind_stairs"
 
-anymal_d_vision_runner_cfg = QuadrupedPPORunnerCfg()
-anymal_d_vision_runner_cfg.experiment_name = "anymal_d_vision"
-anymal_d_vision_runner_cfg.policy.actor_hidden_dims = [512, 256, 128]
-anymal_d_vision_runner_cfg.policy.critic_hidden_dims = [512, 256, 128]
+anymal_d_joints_vision_runner_cfg = QuadrupedPPORunnerCfg()
+anymal_d_joints_vision_runner_cfg.experiment_name = "anymal_d_joints_vision"
+anymal_d_joints_vision_runner_cfg.policy.actor_hidden_dims = [512, 256, 128]
+anymal_d_joints_vision_runner_cfg.policy.critic_hidden_dims = [512, 256, 128]
 
-anymal_d_vision_stairs_runner_cfg = QuadrupedPPORunnerCfg()
-anymal_d_vision_stairs_runner_cfg.experiment_name = "anymal_d_vision_stairs"
-anymal_d_vision_stairs_runner_cfg.policy.actor_hidden_dims = [512, 256, 128]
-anymal_d_vision_stairs_runner_cfg.policy.critic_hidden_dims = [512, 256, 128]
+anymal_d_joints_vision_stairs_runner_cfg = QuadrupedPPORunnerCfg()
+anymal_d_joints_vision_stairs_runner_cfg.experiment_name = "anymal_d_joints_vision_stairs"
+anymal_d_joints_vision_stairs_runner_cfg.policy.actor_hidden_dims = [512, 256, 128]
+anymal_d_joints_vision_stairs_runner_cfg.policy.critic_hidden_dims = [512, 256, 128]
 
 ##
 # Register Gym environments
 ##
 
-#################################
-# Anymal D Blind Flat Environment
-#################################
-
 gym.register(
-    id="Isaac-Quadruped-AnymalD-Blind-Flat-v0",
+    id="Isaac-Quadruped-AnymalD-Joints-Blind-Flat-v0",
     entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": anymal_d_env_cfg.AnymalDBlindFlatEnvCfg,
-        "rsl_rl_cfg_entry_point": anymal_d_blind_flat_runner_cfg,
+        "env_cfg_entry_point": anymal_d_env_cfg.AnymalDJointsBlindFlatEnvCfg,
+        "rsl_rl_cfg_entry_point": anymal_d_joints_blind_flat_runner_cfg,
     },
 )
 
 gym.register(
-    id="Isaac-Quadruped-AnymalD-Blind-Flat-Play-v0",
+    id="Isaac-Quadruped-AnymalD-Joints-Blind-Rough-v0",
     entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": anymal_d_env_cfg.AnymalDBlindFlatEnvCfg_PLAY,
-        "rsl_rl_cfg_entry_point": anymal_d_blind_flat_runner_cfg,
-    },
-)
-
-##################################
-# Anymal D Blind Rough Environment
-##################################
-
-gym.register(
-    id="Isaac-Quadruped-AnymalD-Blind-Rough-v0",
-    entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
-    disable_env_checker=True,
-    kwargs={
-        "env_cfg_entry_point": anymal_d_env_cfg.AnymalDBlindRoughEnvCfg,
-        "rsl_rl_cfg_entry_point": anymal_d_blind_rough_runner_cfg,
+        "env_cfg_entry_point": anymal_d_env_cfg.AnymalDJointsBlindRoughEnvCfg,
+        "rsl_rl_cfg_entry_point": anymal_d_joints_blind_rough_runner_cfg,
     },
 )
 
 gym.register(
-    id="Isaac-Quadruped-AnymalD-Blind-Rough-Play-v0",
+    id="Isaac-Quadruped-AnymalD-Joints-Blind-Stairs-v0",
     entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": anymal_d_env_cfg.AnymalDBlindRoughEnvCfg_PLAY,
-        "rsl_rl_cfg_entry_point": anymal_d_blind_rough_runner_cfg,
-    },
-)
-
-###################################
-# Anymal D Blind Stairs Environment
-###################################
-
-gym.register(
-    id="Isaac-Quadruped-AnymalD-Blind-Stairs-v0",
-    entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
-    disable_env_checker=True,
-    kwargs={
-        "env_cfg_entry_point": anymal_d_env_cfg.AnymalDBlindStairsEnvCfg,
-        "rsl_rl_cfg_entry_point": anymal_d_blind_stairs_runner_cfg,
+        "env_cfg_entry_point": anymal_d_env_cfg.AnymalDJointsBlindStairsEnvCfg,
+        "rsl_rl_cfg_entry_point": anymal_d_joints_blind_stairs_runner_cfg,
     },
 )
 
 gym.register(
-    id="Isaac-Quadruped-AnymalD-Blind-Stairs-Play-v0",
+    id="Isaac-Quadruped-AnymalD-Joints-Vision-v0",
     entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": anymal_d_env_cfg.AnymalDBlindStairsEnvCfg_PLAY,
-        "rsl_rl_cfg_entry_point": anymal_d_blind_stairs_runner_cfg,
-    },
-)
-
-#############################
-# Anymal D Vision Environment
-#############################
-
-gym.register(
-    id="Isaac-Quadruped-AnymalD-Vision-v0",
-    entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
-    disable_env_checker=True,
-    kwargs={
-        "env_cfg_entry_point": anymal_d_env_cfg.AnymalDVisionEnvCfg,
-        "rsl_rl_cfg_entry_point": anymal_d_vision_runner_cfg,
+        "env_cfg_entry_point": anymal_d_env_cfg.AnymalDJointsVisionEnvCfg,
+        "rsl_rl_cfg_entry_point": anymal_d_joints_vision_runner_cfg,
     },
 )
 
 gym.register(
-    id="Isaac-Quadruped-AnymalD-Vision-Play-v0",
+    id="Isaac-Quadruped-AnymalD-Joints-Vision-Stairs-v0",
     entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": anymal_d_env_cfg.AnymalDVisionEnvCfg_PLAY,
-        "rsl_rl_cfg_entry_point": anymal_d_vision_runner_cfg,
+        "env_cfg_entry_point": anymal_d_env_cfg.AnymalDJointsVisionStairsEnvCfg,
+        "rsl_rl_cfg_entry_point": anymal_d_joints_vision_stairs_runner_cfg,
     },
 )
 
-####################################
-# Anymal D Vision Stairs Environment
-####################################
+
+#########################
+# AnymalD CPG Environment
+#########################
+
+##
+# Create PPO runners for RSL-RL
+##
+
+
+# CPG environments
+anymal_d_cpg_blind_flat_runner_cfg = QuadrupedPPORunnerCfg()
+anymal_d_cpg_blind_flat_runner_cfg.experiment_name = "anymal_d_cpg_blind_flat"
+
+anymal_d_cpg_blind_rough_runner_cfg = QuadrupedPPORunnerCfg()
+anymal_d_cpg_blind_rough_runner_cfg.experiment_name = "anymal_d_cpg_blind_rough"
+
+anymal_d_cpg_blind_stairs_runner_cfg = QuadrupedPPORunnerCfg()
+anymal_d_cpg_blind_stairs_runner_cfg.experiment_name = "anymal_d_cpg_blind_stairs"
+
+anymal_d_cpg_vision_runner_cfg = QuadrupedPPORunnerCfg()
+anymal_d_cpg_vision_runner_cfg.experiment_name = "anymal_d_cpg_vision"
+anymal_d_cpg_vision_runner_cfg.policy.actor_hidden_dims = [512, 256, 128]
+anymal_d_cpg_vision_runner_cfg.policy.critic_hidden_dims = [512, 256, 128]
+
+anymal_d_cpg_vision_stairs_runner_cfg = QuadrupedPPORunnerCfg()
+anymal_d_cpg_vision_stairs_runner_cfg.experiment_name = "anymal_d_cpg_vision_stairs"
+anymal_d_cpg_vision_stairs_runner_cfg.policy.actor_hidden_dims = [512, 256, 128]
+anymal_d_cpg_vision_stairs_runner_cfg.policy.critic_hidden_dims = [512, 256, 128]
+
+##
+# Register Gym environments
+##
+
 
 gym.register(
-    id="Isaac-Quadruped-AnymalD-Vision-Stairs-v0",
+    id="Isaac-Quadruped-AnymalD-CPG-Blind-Flat-v0",
     entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": anymal_d_env_cfg.AnymalDVisionStairsEnvCfg,
-        "rsl_rl_cfg_entry_point": anymal_d_vision_stairs_runner_cfg,
+        "env_cfg_entry_point": anymal_d_env_cfg.AnymalDCPGBlindFlatEnvCfg,
+        "rsl_rl_cfg_entry_point": anymal_d_cpg_blind_flat_runner_cfg,
     },
 )
 
 gym.register(
-    id="Isaac-Quadruped-AnymalD-Vision-Stairs-Play-v0",
+    id="Isaac-Quadruped-AnymalD-CPG-Blind-Rough-v0",
     entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": anymal_d_env_cfg.AnymalDVisionStairsEnvCfg_PLAY,
-        "rsl_rl_cfg_entry_point": anymal_d_vision_stairs_runner_cfg,
+        "env_cfg_entry_point": anymal_d_env_cfg.AnymalDCPGBlindRoughEnvCfg,
+        "rsl_rl_cfg_entry_point": anymal_d_cpg_blind_rough_runner_cfg,
+    },
+)
+
+gym.register(
+    id="Isaac-Quadruped-AnymalD-CPG-Blind-Stairs-v0",
+    entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": anymal_d_env_cfg.AnymalDCPGBlindStairsEnvCfg,
+        "rsl_rl_cfg_entry_point": anymal_d_cpg_blind_stairs_runner_cfg,
+    },
+)
+
+gym.register(
+    id="Isaac-Quadruped-AnymalD-CPG-Vision-v0",
+    entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": anymal_d_env_cfg.AnymalDCPGVisionEnvCfg,
+        "rsl_rl_cfg_entry_point": anymal_d_cpg_vision_runner_cfg,
+    },
+)
+
+gym.register(
+    id="Isaac-Quadruped-AnymalD-CPG-Vision-Stairs-v0",
+    entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": anymal_d_env_cfg.AnymalDCPGVisionStairsEnvCfg,
+        "rsl_rl_cfg_entry_point": anymal_d_cpg_vision_stairs_runner_cfg,
     },
 )
--- exts/omni.isaac.lab_quadruped_tasks/omni/isaac/lab_quadruped_tasks/robots/anymal_d/anymal_d_env_cfg.py
+++ exts/omni.isaac.lab_quadruped_tasks/omni/isaac/lab_quadruped_tasks/robots/anymal_d/anymal_d_env_cfg.py
@@ -5,6 +5,7 @@ Copyright (c) 2024, Felipe Mohr Santos
 
 from omni.isaac.lab.utils import configclass
 
+from omni.isaac.lab_quadruped_tasks.robots import base_envs_cfg as base_envs
 from omni.isaac.lab_quadruped_tasks.cfg.quadruped_env_cfg import QuadrupedEnvCfg
 from omni.isaac.lab_quadruped_tasks.cfg.quadruped_terrains_cfg import (
     ROUGH_TERRAINS_CFG,
@@ -20,218 +21,152 @@ from omni.isaac.lab_assets.anymal import ANYMAL_D_CFG
 import math
 
 
-#########################
-# Anymal Base Environment
-#########################
+########################
+# AnymalD Base Environments
+#######################
 
 
 @configclass
-class AnymalDBaseEnvCfg(QuadrupedEnvCfg):
+class AnymalDJointsBaseEnvCfg(base_envs.QuadrupedJointsEnvCfg):
     def __post_init__(self):
         super().__post_init__()
 
         self.scene.robot = ANYMAL_D_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
 
-        self.rewards.rew_feet_air_time.params["sensor_cfg"].body_names = ".*_FOOT"
-        self.rewards.pen_feet_slide.params["sensor_cfg"].body_names = ".*_FOOT"
-        self.rewards.pen_feet_slide.params["asset_cfg"].body_names = ".*_FOOT"
-        self.rewards.pen_undesired_contacts.params["sensor_cfg"].body_names = ".*_THIGH"
+        if self.rewards.rew_feet_air_time is not None:
+            self.rewards.rew_feet_air_time.params["sensor_cfg"].body_names = ".*_FOOT"
+        if self.rewards.pen_feet_slide is not None:
+            self.rewards.pen_feet_slide.params["sensor_cfg"].body_names = ".*_FOOT"
+            self.rewards.pen_feet_slide.params["asset_cfg"].body_names = ".*_FOOT"
+        if self.rewards.pen_undesired_contacts is not None:
+            self.rewards.pen_undesired_contacts.params["sensor_cfg"].body_names = ".*_THIGH"
 
-        self.events.add_base_mass.params["mass_distribution_params"] = (-3.5, 7.0)
+        self.actions.action.scale = 0.5
 
+        self.events.change_actuator_gains = None
 
-@configclass
-class AnymalDBaseEnvCfg_PLAY(AnymalDBaseEnvCfg):
-    def __post_init__(self):
-        super().__post_init__()
-
-        # make a smaller scene for play
-        self.scene.num_envs = 64
-
-        # disable randomization for play
-        self.observations.policy.enable_corruption = False
-        # remove random pushing event
-        self.events.push_robot = None
-        # remove random base mass addition event
-        self.events.add_base_mass = None
-
-
-###############################
-# Anymal Blind Flat Environment
-###############################
+        self.rewards.rew_feet_air_time.weight = 1.25
+        self.rewards.pen_joint_deviation.weight = -0.125
+        self.rewards.pen_undesired_contacts.weight = -1.0
+        self.rewards.pen_feet_slide.weight = -0.1
+        self.rewards.pen_joint_powers.weight = -5e-4
 
 
 @configclass
-class AnymalDBlindFlatEnvCfg(AnymalDBaseEnvCfg):
+class AnymalDCPGBaseEnvCfg(base_envs.QuadrupedCPGEnvCfg):
     def __post_init__(self):
         super().__post_init__()
 
-        self.scene.height_scanner = None
-        self.observations.policy.height_map = None
+        self.scene.robot = ANYMAL_D_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
+        if self.scene.height_scanner is not None:
+            self.scene.height_scanner.prim_path = "{ENV_REGEX_NS}/Robot/body"
+        self.scene.robot.init_state.joint_pos = {
+            ".*hx": 0.0,
+            ".*hy": math.pi / 4,
+            ".*kn": -math.pi / 2,
+        }
 
-        self.events.change_vel_cmd = None
-        self.curriculum.terrain_levels = None
+        if self.events.add_base_mass is not None:
+            self.events.add_base_mass.params["asset_cfg"].body_names = "body"
+        if self.terminations.base_contact is not None:
+            self.terminations.base_contact.params["sensor_cfg"].body_names = "body"
+        if self.rewards.pen_undesired_contacts is not None:
+            self.rewards.pen_undesired_contacts.params["sensor_cfg"].body_names = ".*_uleg"
+
+        # TODO: Fix parameters and QuadrupedIKAction for anymal
+        self.actions.action.front_left_joints = ["fl_.*"]
+        self.actions.action.front_right_joints = ["fr_.*"]
+        self.actions.action.rear_left_joints = ["hl_.*"]
+        self.actions.action.rear_right_joints = ["hr_.*"]
+        self.actions.action.hip_length = 0.11095
+        self.actions.action.thigh_length = 0.3215
+        self.actions.action.calf_length = 0.3365
+        self.actions.action.foot_offset_x = 0.0
+        self.actions.action.foot_offset_y = 0.11095
+        self.actions.action.foot_offset_z = -0.465
+        self.actions.action.step_size = 0.15
+
+        self.events.change_gait = None
+        self.events.change_actuator_gains = None
+
+
+#########################
+# AnymalD Joints Environment
+########################
 
 
 @configclass
-class AnymalDBlindFlatEnvCfg_PLAY(AnymalDBaseEnvCfg_PLAY):
+class AnymalDJointsBlindFlatEnvCfg(AnymalDJointsBaseEnvCfg, base_envs.QuadrupedBlindFlatEnvCfg):
     def __post_init__(self):
-        super().__post_init__()
-
-        self.scene.height_scanner = None
-        self.observations.policy.height_map = None
-
-        self.events.change_vel_cmd = None
-        self.curriculum.terrain_levels = None
-
-
-################################
-# Anymal Blind Rough Environment
-################################
+        AnymalDJointsBaseEnvCfg.__post_init__(self)
+        base_envs.QuadrupedBlindFlatEnvCfg.__post_init__(self)
 
 
 @configclass
-class AnymalDBlindRoughEnvCfg(AnymalDBaseEnvCfg):
+class AnymalDJointsBlindRoughEnvCfg(AnymalDJointsBaseEnvCfg, base_envs.QuadrupedBlindRoughEnvCfg):
     def __post_init__(self):
-        super().__post_init__()
-
-        self.scene.height_scanner = None
-        self.observations.policy.height_map = None
-
-        self.events.change_vel_cmd = None
-
-        self.scene.terrain.terrain_type = "generator"
-        self.scene.terrain.terrain_generator = ROUGH_TERRAINS_CFG
-
-        self.viewer.origin_type = "env"
+        AnymalDJointsBaseEnvCfg.__post_init__(self)
+        base_envs.QuadrupedBlindRoughEnvCfg.__post_init__(self)
 
 
 @configclass
-class AnymalDBlindRoughEnvCfg_PLAY(AnymalDBaseEnvCfg_PLAY):
+class AnymalDJointsBlindStairsEnvCfg(AnymalDJointsBaseEnvCfg, base_envs.QuadrupedBlindStairsEnvCfg):
     def __post_init__(self):
-        super().__post_init__()
-
-        self.scene.height_scanner = None
-        self.observations.policy.height_map = None
-
-        self.events.change_vel_cmd = None
-
-        self.scene.terrain.terrain_type = "generator"
-        self.scene.terrain.max_init_terrain_level = None
-        self.scene.terrain.terrain_generator = ROUGH_TERRAINS_PLAY_CFG
-
-
-#################################
-# Anymal Blind Stairs Environment
-#################################
+        AnymalDJointsBaseEnvCfg.__post_init__(self)
+        base_envs.QuadrupedBlindStairsEnvCfg.__post_init__(self)
 
 
 @configclass
-class AnymalDBlindStairsEnvCfg(AnymalDBaseEnvCfg):
+class AnymalDJointsVisionEnvCfg(AnymalDJointsBaseEnvCfg, base_envs.QuadrupedVisionEnvCfg):
     def __post_init__(self):
-        super().__post_init__()
+        AnymalDJointsBaseEnvCfg.__post_init__(self)
+        base_envs.QuadrupedVisionEnvCfg.__post_init__(self)
 
-        self.scene.height_scanner = None
-        self.observations.policy.height_map = None
 
-        self.commands.base_velocity.ranges.lin_vel_x = (0.5, 1.0)
-        self.commands.base_velocity.ranges.lin_vel_y = (0.0, 0.0)
-        self.commands.base_velocity.ranges.ang_vel_z = (-math.pi / 6, math.pi / 6)
+class AnymalDJointsVisionStairsEnvCfg(AnymalDJointsBaseEnvCfg, base_envs.QuadrupedVisionStairsEnvCfg):
+    def __post_init__(self):
+        AnymalDJointsBaseEnvCfg.__post_init__(self)
+        base_envs.QuadrupedVisionStairsEnvCfg.__post_init__(self)
 
-        self.events.reset_robot_base.params["pose_range"]["yaw"] = (0.0, 0.0)
 
-        self.scene.terrain.terrain_type = "generator"
-        self.scene.terrain.terrain_generator = STAIRS_TERRAINS_CFG
-
-        self.viewer.origin_type = "env"
+######################
+# AnymalD CPG Environment
+#####################
 
 
 @configclass
-class AnymalDBlindStairsEnvCfg_PLAY(AnymalDBaseEnvCfg_PLAY):
+class AnymalDCPGBlindFlatEnvCfg(AnymalDCPGBaseEnvCfg, base_envs.QuadrupedBlindFlatEnvCfg):
     def __post_init__(self):
-        super().__post_init__()
-
-        self.scene.height_scanner = None
-        self.observations.policy.height_map = None
-
-        self.commands.base_velocity.ranges.lin_vel_x = (0.5, 1.0)
-        self.commands.base_velocity.ranges.lin_vel_y = (0.0, 0.0)
-        self.commands.base_velocity.ranges.ang_vel_z = (0.0, 0.0)
-
-        self.events.reset_robot_base.params["pose_range"]["yaw"] = (0.0, 0.0)
-
-        self.scene.terrain.terrain_type = "generator"
-        self.scene.terrain.max_init_terrain_level = None
-        self.scene.terrain.terrain_generator = STAIRS_TERRAINS_PLAY_CFG
-
-
-############################
-# AnymalD Vision Environment
-############################
+        AnymalDCPGBaseEnvCfg.__post_init__(self)
+        base_envs.QuadrupedBlindFlatEnvCfg.__post_init__(self)
+        self.actions.action.use_joints_offset = False
+        self.rewards.pen_offset_joints = None
 
 
 @configclass
-class AnymalDVisionEnvCfg(AnymalDBaseEnvCfg):
+class AnymalDCPGBlindRoughEnvCfg(AnymalDCPGBaseEnvCfg, base_envs.QuadrupedBlindRoughEnvCfg):
     def __post_init__(self):
-        super().__post_init__()
-
-        self.scene.terrain.terrain_type = "generator"
-        self.scene.terrain.terrain_generator = FULL_TERRAINS_CFG
-
-        self.events.change_vel_cmd = None
-
-        self.viewer.origin_type = "env"
+        AnymalDCPGBaseEnvCfg.__post_init__(self)
+        base_envs.QuadrupedBlindRoughEnvCfg.__post_init__(self)
+        self.events.change_gait = None
+        self.actions.action.gait_type = "walk"
 
 
 @configclass
-class AnymalDVisionEnvCfg_PLAY(AnymalDBaseEnvCfg_PLAY):
+class AnymalDCPGBlindStairsEnvCfg(AnymalDCPGBaseEnvCfg, base_envs.QuadrupedBlindStairsEnvCfg):
     def __post_init__(self):
-        super().__post_init__()
-
-        self.scene.terrain.terrain_type = "generator"
-        self.scene.terrain.max_init_terrain_level = None
-        self.scene.terrain.terrain_generator = FULL_TERRAINS_PLAY_CFG
-
-        self.events.change_vel_cmd = None
-
-        self.viewer.origin_type = "env"
-
-
-##################################
-# AnymalD Vision Stairs Environment
-###################################
+        AnymalDCPGBaseEnvCfg.__post_init__(self)
+        base_envs.QuadrupedBlindStairsEnvCfg.__post_init__(self)
 
 
 @configclass
-class AnymalDVisionStairsEnvCfg(AnymalDBaseEnvCfg):
+class AnymalDCPGVisionEnvCfg(AnymalDCPGBaseEnvCfg, base_envs.QuadrupedVisionEnvCfg):
     def __post_init__(self):
-        super().__post_init__()
-
-        self.commands.base_velocity.ranges.lin_vel_x = (0.5, 1.0)
-        self.commands.base_velocity.ranges.lin_vel_y = (0.0, 0.0)
-        self.commands.base_velocity.ranges.ang_vel_z = (-math.pi / 6, math.pi / 6)
-
-        self.events.reset_robot_base.params["pose_range"]["yaw"] = (0.0, 0.0)
-
-        self.scene.terrain.terrain_type = "generator"
-        self.scene.terrain.terrain_generator = STAIRS_TERRAINS_CFG
-
-        self.viewer.origin_type = "env"
+        AnymalDCPGBaseEnvCfg.__post_init__(self)
+        base_envs.QuadrupedVisionEnvCfg.__post_init__(self)
 
 
-@configclass
-class AnymalDVisionStairsEnvCfg_PLAY(AnymalDBaseEnvCfg_PLAY):
+class AnymalDCPGVisionStairsEnvCfg(AnymalDCPGBaseEnvCfg, base_envs.QuadrupedVisionStairsEnvCfg):
     def __post_init__(self):
-        super().__post_init__()
-
-        self.commands.base_velocity.ranges.lin_vel_x = (0.5, 1.0)
-        self.commands.base_velocity.ranges.lin_vel_y = (0.0, 0.0)
-        self.commands.base_velocity.ranges.ang_vel_z = (0.0, 0.0)
-
-        self.events.reset_robot_base.params["pose_range"]["yaw"] = (0.0, 0.0)
-
-        self.scene.terrain.terrain_type = "generator"
-        self.scene.terrain.max_init_terrain_level = None
-        self.scene.terrain.terrain_generator = STAIRS_TERRAINS_PLAY_CFG
-
-        self.viewer.origin_type = "env"
+        AnymalDCPGBaseEnvCfg.__post_init__(self)
+        base_envs.QuadrupedVisionStairsEnvCfg.__post_init__(self)
