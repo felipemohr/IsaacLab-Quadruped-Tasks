15e21d1103032caef4a859ff8b123b2bdaf4d86d
Felipe Mohr <felipe18mohr@gmail.com>
2025-01-17 23:59:14 -0300

    Update change_gait_type event to set desired gaits

--- exts/omni.isaac.lab_quadruped_tasks/omni/isaac/lab_quadruped_tasks/mdp/events.py
+++ exts/omni.isaac.lab_quadruped_tasks/omni/isaac/lab_quadruped_tasks/mdp/events.py
@@ -6,6 +6,7 @@ Copyright (c) 2024, Felipe Mohr Santos
 from __future__ import annotations
 
 import torch
+from collections.abc import Sequence
 from typing import TYPE_CHECKING
 
 from omni.isaac.lab_quadruped_tasks.mdp.actions.quadruped_actions import QuadrupedCPGAction
@@ -25,11 +26,20 @@ def invert_vel_cmd(env: ManagerBasedRLEnv, env_ids: torch.Tensor | None, command
     cmd_cfg.cfg.ranges.lin_vel_x = (-max_vel, -min_vel)
 
 
-def change_gait_type(env: ManagerBasedRLEnv, env_ids: torch.Tensor | None, action_name: str):
+def change_gait_type(
+    env: ManagerBasedRLEnv, env_ids: torch.Tensor | None, action_name: str, gaits: Sequence[str] | None = None
+):
     """ """
     # extract the used quantities (to enable type-hinting)
     if env_ids is None:
         env_ids = torch.arange(env.scene.num_envs, device=env.device)
     cpg_action: QuadrupedCPGAction = env.action_manager.get_term(action_name)
-    gaits = torch.randint_like(env_ids, 0, len(cpg_action.supported_gaits) - 1)
-    cpg_action.set_gait_type(gaits, env_ids)
+
+    if gaits == None:
+        gait_idxs = torch.randint_like(env_ids, 0, len(cpg_action.supported_gaits))
+    else:
+        gaits_to_use = torch.tensor([cpg_action.supported_gaits.index(gait) for gait in gaits], device=env.device)
+        random_gaits = torch.randint_like(env_ids, 0, gaits_to_use.size(0))
+        gait_idxs = gaits_to_use[random_gaits]
+
+    cpg_action.set_gait_type(gait_idxs, env_ids)
